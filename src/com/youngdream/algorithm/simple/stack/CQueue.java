3a40e6974d6506f9ac82914aed16818d38993a31
youngdream <994300880@qq.com>
2020-05-23 14:42:24 +0800

    no message

--- src/com/youngdream/algorithm/simple/stack/CQueue.java
+++ src/com/youngdream/algorithm/simple/stack/CQueue.java
@@ -7,6 +7,25 @@ import java.util.Stack;
  * @date 2020/5/23 14:18
  */
 public class CQueue {
+    /**
+     * 用两个栈实现一个队列。队列的声明如下，请实现它的两个函数 appendTail 和 deleteHead ，分别完成在队列尾部插入整数和在队列头部删除整数的功能。(若队列中没有元素，deleteHead 操作返回 -1 )
+     * <p>
+     * 示例 1：
+     * 输入：
+     * ["CQueue","appendTail","deleteHead","deleteHead"]
+     * [[],[3],[],[]]
+     * 输出：[null,null,3,-1]
+     * <p>
+     * 示例 2：
+     * 输入：
+     * ["CQueue","deleteHead","appendTail","appendTail","deleteHead","deleteHead"]
+     * [[],[],[5],[2],[],[]]
+     * 输出：[null,-1,null,null,5,2]
+     * <p>
+     * 提示：
+     * 1 <= values <= 10000
+     * 最多会对 appendTail、deleteHead 进行 10000 次调用
+     */
     private Stack<Integer> inStack;
     private Stack<Integer> outStack;
 
